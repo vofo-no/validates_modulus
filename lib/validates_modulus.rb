89c4aaa4838fa0d363ce42089ba8f5d35a613afe
Mats Grimsgaard <mg@vofo.no>
2019-03-26 11:49:35 +0000

    Initial commit

---
+++ lib/validates_modulus.rb
@@ -0,0 +1,6 @@
+require "validates_modulus/version"
+
+module ValidatesModulus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/validates_modulus/version.rb
@@ -0,0 +1,3 @@
+module ValidatesModulus
+  VERSION = "0.1.0"
+end
